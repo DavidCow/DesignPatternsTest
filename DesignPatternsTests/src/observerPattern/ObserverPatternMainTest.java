155a7af83f4f1a31de2ae876ab7f2d801ed81def
David Cao <hngnar@hotmail.com>
2016-11-18 19:27:39 +0100

    Observer Package

---
+++ DesignPatternsTests/src/observerPattern/ObserverPatternMainTest.java
@@ -0,0 +1,14 @@
+package observerPattern;
+
+/**
+ * Observer implementation testing
+ * @author David
+ *
+ */
+public class ObserverPatternMainTest {
+	
+	public static void main(String[] args) {
+
+	}
+	
+}
